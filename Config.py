66b008f0704d4f22faa1c5c0e1e813a4fc4112de
Shreyas-Sudarshanam <shreyas.sudarshanam98@gmail.com>
2026-03-18 00:18:01 +0000

    Create Config.py
    Initial commit

---
+++ Config.py
@@ -0,0 +1,12 @@
+Random_State=42
+test_size=0.2
+#minimum instances reqired for a class 
+min_instance=5
+#classification columns
+Type_columns=['Type2','Type3','Type4']
+#type1 class is not included as it has only 1 class
+text_column='email_text'
+#vectorization setting 
+TFIDF_MAX_FEATURES=5000
+TFIDF_NGRAM_RANGE=(1,2)
+#parameters
